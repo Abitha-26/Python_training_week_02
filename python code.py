35328fa65fcb2a9a7ccb249a4cb25ef395125a3c
Abitha <abithad61@gmail.com>
2025-01-25 18:13:27 +0530

    Add files via upload

---
+++ python code.py
@@ -0,0 +1,14 @@
+
+class ClassName:
+    def __init__(self, name1, age2):
+        self.name = name1
+        self.age = age2
+
+    def method_name(self):
+        print(f"name: {self.name}, age: {self.age}")
+
+# Creating an instance of ClassName
+obj = ClassName("Abitha", 19)
+
+# Calling the method
+obj.method_name()
